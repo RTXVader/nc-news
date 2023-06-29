06f832ef953dbe9d34e70b355d78c28c741774f1
Your Name <you@example.com>
2023-06-29 14:45:52 +0100

    kata 5 solution

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const app = express();
-const { getTopics, getDescriptions,getArticleId } = require("./controller/app.controller");
+const { getTopics, getDescriptions,getArticleId, getArticles } = require("./controller/app.controller");
 const { handlePsqlErrors, handleDefaultErrors } = require('./app.errors')
 app.get("/api/topics", getTopics);
 app.get("/api", getDescriptions);
 app.get('/api/articles/:article_id', getArticleId)
+app.get('/api/articles', getArticles)
 app.use(handlePsqlErrors)
 app.use(handleDefaultErrors)
 module.exports = app;
